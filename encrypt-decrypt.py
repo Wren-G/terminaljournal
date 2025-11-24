3af6be481aa2fba37d565e6647a6f2bdfa012d53
Wren-G <westre@oregonstate.edu>
2025-11-23 16:47:37 -0800

    changed encrypt

--- encrypt-decrypt.py
+++ encrypt-decrypt.py
@@ -1,26 +1,19 @@
-#This is the microservice to encrypt and decrypt text files :)
-
-#libraries
 import time
 import os
 
-#Global variables
 charType = ''
 publicKeys = {
     # add in a set of (n, e) keys here (ex. these are small n!)
-    # 4460543: 3791
     444446005879: 679
 }
 privateKeys = {
     # add in a set of (n, d) keys here (ex. for small n)
-    # 4460543: 2351
     444446005879: 1309115239
 }
 
-#This function will encrypt the text
+# Encrypt text
 def encrypt(rawTxt, outputPath):
     # establish lists
-    # print('Encrypting...')
     
     chunkList = []
     encrypted_chunks = []
@@ -49,7 +42,7 @@ def encrypt(rawTxt, outputPath):
         f.write(to_write)
     return to_write
 
-#This function will decrypt the text using a key
+# Decrypt the text using a key
 def decrypt(rawTxt, outputPath):
     # divide the string from file into numbers
     strs = rawTxt.split()
@@ -135,31 +128,30 @@ def decode(n):
         # concatenate all elements together
         my_string += char_for_str
     
-    # print(my_string)
     return my_string
 
 def main():
     baseDir = os.path.dirname(os.path.abspath(__file__))
     inputPath = os.path.join(baseDir, "input.txt")
     outputPath = os.path.join(baseDir, "output.txt")
-    while True: #This loops reads the txt file once per second
-        #sleep while waiting for input.txt
+    while True: # This loops reads the txt file once per second
+        # sleep while waiting for input.txt
         with open(inputPath, "r") as f: 
             rawTxt = f.read() #rawTxt is just a variable holding the text!
         
-        if not rawTxt: #if nothing was read...
-            time.sleep(1) #sleeps for one second
-            continue #start over
+        if not rawTxt: # if nothing read, sleep then repeat
+            time.sleep(1)
+            continue
 
-        #once input.txt has text in it,
-        #take the first char off the file and store in charType
+        # once input.txt has text in it,
+        # take the first char off the file and store in charType
         charType = rawTxt[0]
-        #remove the first character from the text file
-        rawTxt = rawTxt[1:] #this just leaves the rest of the string minus that first char
+        # remove the first character from the text file
+        rawTxt = rawTxt[1:] # this just leaves the rest of the string minus that first char
 
         with open(inputPath, "w") as f:
-            f.write("") #this erases the input text so that the program does not get confused
-            #Don't worry! we already saved the important information in rawTxt
+            f.write("") # this erases the input text so that the program does not get confused
+            # (important information saved in rawTxt)
 
         if (charType == 'e'): #first char e = encryption mode
             encrypt(rawTxt, outputPath)
